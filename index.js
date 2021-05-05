50a2ea979edda27132cbbb29e1564772e69c9bf3
akinloludavid <67186221+akinloludavid@users.noreply.github.com>
2021-05-05 07:46:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const http = require('http')
 const fs = require('fs')
 const express = require('express')
-const movies = require('./data/database')
 const app = express()
-const Joi = require('joi')
-const movieRouter = require('./routes/allmovies')
+const customer = require('./routes/customers')
+const genre = require('./routes/genres')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/vidlyapp')
+  .then(() => console.log('Connected to DB'))
+  .catch((err) => console.log('Error in database connection', err))
+
 
 app.use(express.json())
-app.use('/movies', movieRouter)
+app.use('/customers', customer)
+app.use('/genres', genre)
 const port = process.env.PORT || 4500
 
 app.listen(port, ()=>{
