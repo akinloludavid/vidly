3b2ef630ef095ad3061406da118dc1a16f27f4c0
akinloludavid <akinloludavid2727@gmail.com>
2021-06-08 09:19:26 +0100

    added test file

---
+++ __test__/index.test.js
@@ -0,0 +1,5 @@
+const index = require('../index')
+
+test('Testing microphone', ()=>{
+  expect(1).toBe(1)
+})
--- models/genre.js
+++ models/genre.js
@@ -1,10 +1,25 @@
-const mongoose = require('mongoose')
+const Joi = require('joi');
+const mongoose = require('mongoose');
 
-const movieSchema = new mongoose.Schema({
-  title: String,
-  genre: String,
-  rating: Number
-})
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+});
 
-const Moviedb = mongoose.model('Movies', movieSchema)
-module.exports = Moviedb;
+const Genre = mongoose.model('Genre', genreSchema);
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+exports.genreSchema = genreSchema;
+exports.Genre = Genre; 
+exports.validate = validateGenre;
